983f47baaffeccb3e9b38c77a92f8ae7915afb50
Pau Tallada Crespí <pau.tallada@gmail.com>
2013-01-28 23:06:00 +0100

    changes

---
+++ brownthrower/manager/commands/task.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import prettytable
+import textwrap
+
+from base import Command
+
+class TaskShow(Command):
+    
+    def __init__(self, tasks, *args, **kwargs):
+        super(TaskShow, self).__init__(*args, **kwargs)
+        self._tasks = tasks
+    
+    def help(self, items):
+        print textwrap.dedent("""\
+        usage: task show [name] ...
+        
+        Show a list of all the tasks available in this environment.
+        If a 'name' is supplied, show a detailed description of that task.
+        """)
+    
+    def do(self, items):
+        if not items:
+            if len(self._tasks) == 0:
+                print "There are no tasks currently registered in this environment."
+                return
+            
+            t = prettytable.PrettyTable(['name', 'description'])
+            for name, task in self._tasks.iteritems():
+                t.add_row([name, task.get_help()[0]])
+            
+            print t
+            return
+        
+        # Show the details of one or more tasks
+        for item in items:
+            task = self._tasks.get(item)
+            if task:
+                desc = task.get_help()
+                header = "Details for the task '%s'" % item
+                print
+                print header
+                print "=" * len(header)
+                print desc[0]
+                print
+                print desc[1]
+            else:
+                print "The task '%s' is not currently available in this environment."
+                print
+    
+    def complete(self, text, items):
+        matching = set([key
+                        for key in self._tasks.iterkeys()
+                        if key.startswith(text)])
+        
+        return list(matching - set(items))
+
+class Task(Command):
+    
+    def __init__(self, tasks, *args, **kwargs):
+        super(Task, self).__init__(*args, **kwargs)
+        
+        self.add_subcmd('show', TaskShow(tasks = tasks))
+    
+    def help(self, items):
+        print textwrap.dedent("""\
+        usage: task <command> [options]
+        
+        Available commands:
+            show        show detailed information for a task
+        """)
+    
+    def complete(self, text, items):
+        available = self._subcmds.keys()
+        
+        return [command
+                for command in available
+                if command.startswith(text)]
+    
+    def do(self, items):
+        self.help(items)
