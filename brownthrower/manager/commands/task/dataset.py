b5f175bef78b5f161ad681ffde31a97f4920afd1
Pau Tallada Crespí <pau.tallada@gmail.com>
2013-05-30 10:36:37 +0200

    Reorder methods

--- brownthrower/manager/commands/task/dataset.py
+++ brownthrower/manager/commands/task/dataset.py
@@ -64,218 +64,6 @@ class TaskDatasetAttr(Command):
             finally:
                 log.debug(e)
 
-class TaskDatasetShow(Command):
-    @property
-    def __doc__(self):
-        return """\
-        usage: task {dataset} show <task> <name>
-        
-        Show the {dataset} dataset with the specified name for a given task.
-        """.format(
-            dataset = self._dataset
-        )
-    
-    def __init__(self, dataset, *args, **kwargs):
-        super(TaskDatasetShow, self).__init__(*args, **kwargs)
-        self._dataset = dataset
-        self._profile = getattr(api.profile, self._dataset)
-    
-    def complete(self, text, items):
-        if not items:
-            matching = [key
-                        for key in api.get_tasks().iterkeys()
-                        if key.startswith(text)]
-            return matching
-        
-        if (len(items) == 1) and (items[0] in api.get_tasks().iterkeys()):
-            matching = [key
-                        for key in self._profile.get_available(items[0])
-                        if key.startswith(text)]
-            
-            return matching
-    
-    def do(self, items):
-        if len(items) != 2:
-            return self.help(items)
-        
-        try:
-            path = self._profile.get_dataset_path(items[0], items[1])
-            assert os.access(path, os.R_OK)
-            subprocess.check_call([settings['pager'], path])
-        
-        except Exception as e:
-            try:
-                raise
-            except AssertionError:
-                error("Cannot open this dataset for reading.")
-            except api.dataset.NoProfileIsActive:
-                error("No configuration profile is active at this time. Please, switch into one.")
-            except subprocess.CalledProcessError:
-                error("An error occurred while trying to display this dataset.")
-            finally:
-                log.debug(e)
-
-class TaskDatasetEdit(Command):
-    @property
-    def __doc__(self):
-        return """\
-        usage: task {dataset} edit <task> <name>
-        
-        Edit the {dataset} dataset with the specified name for a given task.
-        """.format(
-            dataset = self._dataset
-        )
-    
-    def __init__(self, dataset, *args, **kwargs):
-        super(TaskDatasetEdit, self).__init__(*args, **kwargs)
-        self._dataset   = dataset
-        self._profile   = getattr(api.profile, self._dataset)
-        self._validator = api.task.get_validator(dataset)
-    
-    def complete(self, text, items):
-        if not items:
-            matching = [key
-                        for key in api.get_tasks().iterkeys()
-                        if key.startswith(text)]
-            return matching
-        
-        if (len(items) == 1) and (items[0] in api.get_tasks().iterkeys()):
-            matching = [key
-                        for key in self._profile.get_available(items[0])
-                        if key.startswith(text)]
-            
-            return matching
-    
-    def do(self, items):
-        if len(items) != 2:
-            return self.help(items)
-        
-        try:
-            path = self._profile.get_dataset_path(items[0], items[1])
-            assert os.access(path, os.W_OK)
-            task = api.get_task(items[0])
-            
-            with tempfile.NamedTemporaryFile("w+") as fh:
-                shutil.copyfileobj(open(path), fh)
-                fh.flush()
-                
-                subprocess.check_call([settings['editor'], fh.name])
-                
-                fh.seek(0)
-                self._validator(task, fh.read())
-                fh.seek(0)
-                shutil.copyfileobj(fh, open(path, 'w'))
-            
-            success("The dataset has been successfully modified.")
-        
-        except Exception as e:
-            try:
-                raise
-            except api.task.UnavailableException:
-                error("The task '%s' is not available in this environment." % e.task)
-            except api.dataset.NoProfileIsActive:
-                error("No configuration profile is active at this time. Please, switch into one.")
-            except AssertionError:
-                error("Cannot open this dataset for writing.")
-            except EnvironmentError:
-                error("Unable to open the temporary dataset buffer.")
-            except subprocess.CalledProcessError:
-                error("An error occurred while trying to edit this dataset.")
-            except api.task.ValidationException:
-                error("The new value for this dataset is not valid.")
-            finally:
-                log.debug(e)
-
-class TaskDatasetRemove(Command):
-    @property
-    def __doc__(self):
-        return """\
-        usage: task {dataset} remove <task> <name>
-        
-        Delete the {dataset} dataset with the specified name for a given task.
-        """.format(
-            dataset = self._dataset
-        )
-    
-    def __init__(self, dataset, *args, **kwargs):
-        super(TaskDatasetRemove, self).__init__(*args, **kwargs)
-        self._dataset = dataset
-        self._profile = getattr(api.profile, self._dataset)
-    
-    def complete(self, text, items):
-        if not items:
-            matching = [key
-                        for key in api.get_tasks().iterkeys()
-                        if key.startswith(text)]
-            return matching
-        
-        if (len(items) == 1) and (items[0] in api.get_tasks().iterkeys()):
-            matching = [key
-                        for key in self._profile.get_available(items[0])
-                        if key.startswith(text)]
-            
-            return matching
-    
-    def do(self, items):
-        if len(items) != 2:
-            return self.help(items)
-        
-        try:
-            self._profile.remove(items[0], items[1])
-            success("The dataset has been successfully removed.")
-        
-        except Exception as e:
-            try:
-                raise
-            except api.profile.DoesNotExistError:
-                error("There is no %s dataset named '%s' for the given task '%s'." % (self._dataset, items[1], items[0]))
-            except api.dataset.NoProfileIsActive:
-                error("No configuration profile is active at this time. Please, switch into one.")
-            finally:
-                log.debug(e)
-
-class TaskDatasetList(Command):
-    @property
-    def __doc__(self):
-        return """\
-        usage: task {dataset} list <task>
-        
-        Show a list of all the {dataset} datasets available for the given task.
-        """.format(
-            dataset = self._dataset
-        )
-    
-    def __init__(self, dataset, *args, **kwargs):
-        super(TaskDatasetList, self).__init__(*args, **kwargs)
-        self._dataset = dataset
-        self._profile = getattr(api.profile, self._dataset)
-    
-    def complete(self, text, items):
-        if not items:
-            matching = [key
-                        for key in api.get_tasks().iterkeys()
-                        if key.startswith(text)]
-            
-            return matching
-    
-    def do(self, items):
-        if len(items) != 1:
-            return self.help(items)
-        
-        datasets = self._profile.get_available(items[0]) # @UndefinedVariable
-        default = self._profile.get_default(items[0])   # @UndefinedVariable
-        
-        if len(datasets) == 0:
-            warn("There are no %s datasets currently available for this task." % self._dataset)
-            return
-        
-        table = []
-        for name in sorted(datasets):
-            table.append([name, 'D' if name == default else ''])
-        
-        print tabulate(table, headers=['name', ''])
-        if datasets:
-            print strong("'D' in the second column designates the Default dataset.")
 
 class TaskDatasetCreate(Command):
     @property
@@ -437,5 +225,219 @@ class TaskDatasetDefault(Command):
                 error("There is no %s dataset named '%s' for the task '%s'." % (self._dataset, items[1], items[0]))
             except api.dataset.NoProfileIsActive:
                 error("No configuration profile is active at this time. Please, switch into one.")
+            finally:
+                log.debug(e)
+
+class TaskDatasetEdit(Command):
+    @property
+    def __doc__(self):
+        return """\
+        usage: task {dataset} edit <task> <name>
+        
+        Edit the {dataset} dataset with the specified name for a given task.
+        """.format(
+            dataset = self._dataset
+        )
+    
+    def __init__(self, dataset, *args, **kwargs):
+        super(TaskDatasetEdit, self).__init__(*args, **kwargs)
+        self._dataset   = dataset
+        self._profile   = getattr(api.profile, self._dataset)
+        self._validator = api.task.get_validator(dataset)
+    
+    def complete(self, text, items):
+        if not items:
+            matching = [key
+                        for key in api.get_tasks().iterkeys()
+                        if key.startswith(text)]
+            return matching
+        
+        if (len(items) == 1) and (items[0] in api.get_tasks().iterkeys()):
+            matching = [key
+                        for key in self._profile.get_available(items[0])
+                        if key.startswith(text)]
+            
+            return matching
+    
+    def do(self, items):
+        if len(items) != 2:
+            return self.help(items)
+        
+        try:
+            path = self._profile.get_dataset_path(items[0], items[1])
+            assert os.access(path, os.W_OK)
+            task = api.get_task(items[0])
+            
+            with tempfile.NamedTemporaryFile("w+") as fh:
+                shutil.copyfileobj(open(path), fh)
+                fh.flush()
+                
+                subprocess.check_call([settings['editor'], fh.name])
+                
+                fh.seek(0)
+                self._validator(task, fh.read())
+                fh.seek(0)
+                shutil.copyfileobj(fh, open(path, 'w'))
+            
+            success("The dataset has been successfully modified.")
+        
+        except Exception as e:
+            try:
+                raise
+            except api.task.UnavailableException:
+                error("The task '%s' is not available in this environment." % e.task)
+            except api.dataset.NoProfileIsActive:
+                error("No configuration profile is active at this time. Please, switch into one.")
+            except AssertionError:
+                error("Cannot open this dataset for writing.")
+            except EnvironmentError:
+                error("Unable to open the temporary dataset buffer.")
+            except subprocess.CalledProcessError:
+                error("An error occurred while trying to edit this dataset.")
+            except api.task.ValidationException:
+                error("The new value for this dataset is not valid.")
+            finally:
+                log.debug(e)
+
+
+class TaskDatasetList(Command):
+    @property
+    def __doc__(self):
+        return """\
+        usage: task {dataset} list <task>
+        
+        Show a list of all the {dataset} datasets available for the given task.
+        """.format(
+            dataset = self._dataset
+        )
+    
+    def __init__(self, dataset, *args, **kwargs):
+        super(TaskDatasetList, self).__init__(*args, **kwargs)
+        self._dataset = dataset
+        self._profile = getattr(api.profile, self._dataset)
+    
+    def complete(self, text, items):
+        if not items:
+            matching = [key
+                        for key in api.get_tasks().iterkeys()
+                        if key.startswith(text)]
+            
+            return matching
+    
+    def do(self, items):
+        if len(items) != 1:
+            return self.help(items)
+        
+        datasets = self._profile.get_available(items[0]) # @UndefinedVariable
+        default = self._profile.get_default(items[0])   # @UndefinedVariable
+        
+        if len(datasets) == 0:
+            warn("There are no %s datasets currently available for this task." % self._dataset)
+            return
+        
+        table = []
+        for name in sorted(datasets):
+            table.append([name, 'D' if name == default else ''])
+        
+        print tabulate(table, headers=['name', ''])
+        if datasets:
+            print strong("'D' in the second column designates the Default dataset.")
+
+class TaskDatasetRemove(Command):
+    @property
+    def __doc__(self):
+        return """\
+        usage: task {dataset} remove <task> <name>
+        
+        Delete the {dataset} dataset with the specified name for a given task.
+        """.format(
+            dataset = self._dataset
+        )
+    
+    def __init__(self, dataset, *args, **kwargs):
+        super(TaskDatasetRemove, self).__init__(*args, **kwargs)
+        self._dataset = dataset
+        self._profile = getattr(api.profile, self._dataset)
+    
+    def complete(self, text, items):
+        if not items:
+            matching = [key
+                        for key in api.get_tasks().iterkeys()
+                        if key.startswith(text)]
+            return matching
+        
+        if (len(items) == 1) and (items[0] in api.get_tasks().iterkeys()):
+            matching = [key
+                        for key in self._profile.get_available(items[0])
+                        if key.startswith(text)]
+            
+            return matching
+    
+    def do(self, items):
+        if len(items) != 2:
+            return self.help(items)
+        
+        try:
+            self._profile.remove(items[0], items[1])
+            success("The dataset has been successfully removed.")
+        
+        except Exception as e:
+            try:
+                raise
+            except api.profile.DoesNotExistError:
+                error("There is no %s dataset named '%s' for the given task '%s'." % (self._dataset, items[1], items[0]))
+            except api.dataset.NoProfileIsActive:
+                error("No configuration profile is active at this time. Please, switch into one.")
+            finally:
+                log.debug(e)
+
+class TaskDatasetShow(Command):
+    @property
+    def __doc__(self):
+        return """\
+        usage: task {dataset} show <task> <name>
+        
+        Show the {dataset} dataset with the specified name for a given task.
+        """.format(
+            dataset = self._dataset
+        )
+    
+    def __init__(self, dataset, *args, **kwargs):
+        super(TaskDatasetShow, self).__init__(*args, **kwargs)
+        self._dataset = dataset
+        self._profile = getattr(api.profile, self._dataset)
+    
+    def complete(self, text, items):
+        if not items:
+            matching = [key
+                        for key in api.get_tasks().iterkeys()
+                        if key.startswith(text)]
+            return matching
+        
+        if (len(items) == 1) and (items[0] in api.get_tasks().iterkeys()):
+            matching = [key
+                        for key in self._profile.get_available(items[0])
+                        if key.startswith(text)]
+            
+            return matching
+    
+    def do(self, items):
+        if len(items) != 2:
+            return self.help(items)
+        
+        try:
+            path = self._profile.get_dataset_path(items[0], items[1])
+            assert os.access(path, os.R_OK)
+            subprocess.check_call([settings['pager'], path])
+        
+        except Exception as e:
+            try:
+                raise
+            except AssertionError:
+                error("Cannot open this dataset for reading.")
+            except api.dataset.NoProfileIsActive:
+                error("No configuration profile is active at this time. Please, switch into one.")
+            except subprocess.CalledProcessError:
+                error("An error occurred while trying to display this dataset.")
             finally:
                 log.debug(e)
